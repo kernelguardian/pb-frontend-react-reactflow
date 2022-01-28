e2da96ed1013abb56d1982217f47421596562fcf
Asish Binu Mathew <asish.bm@devbud.ai>
2022-01-29 03:55:32 +0530

    defined folder structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import Flowbuilder from "./reactflow/Flowbuilder";
+import Input from "./foundation/Input";
+import Button from "./foundation/Button";
+import Text from "./foundation/Text";
+import Navbar from "./screen/Navbar";
 
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <Navbar></Navbar>
+      <h1>Flowbuilder</h1>
+      <Flowbuilder></Flowbuilder>
+
+    </div>
+
   );
 }
 
