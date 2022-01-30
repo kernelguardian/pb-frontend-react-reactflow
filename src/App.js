d38208db0a2bd5ed7e7f0c4e3db856757e541d01
Asish Binu Mathew <asish.bm@devbud.ai>
2022-01-31 02:14:26 +0530

    Removed initial node

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import NodeList from "./screen/NodeList";
 import { useState } from "react"
 
 function App() {
-  const [activeNode, setactiveNode] = useState([{
-    id: '100',
-    type: 'input', // input node
-    data: { label: 'Input Node' },
-    position: { x: 250, y: 25 },
-  }]);
+  const [activeNode, setactiveNode] = useState([]);
 
   return (
     <div>
