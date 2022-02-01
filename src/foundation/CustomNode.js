e0f571ce229fdbf137dbc5f618ede43cbb8dd32d
Asish Binu Mathew <asish.bm@devbud.ai>
2022-02-01 15:47:30 +0530

    Created CustomNode

---
+++ src/foundation/CustomNode.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Button from './Button';
+import Card from './Card';
+import Input from './Input';
+import Text from './Text';
+
+
+
+const CustomNodeComponent = (props) => {
+    return (
+        <div >
+
+            <div>{props.data.name}</div>
+            <Input placeholder="Type something ?"></Input>
+            <Text text="Text to be displayed"></Text>
+            <Button buttonText="Submit"></Button>
+        </div>
+    );
+};
+
+export default CustomNodeComponent;
